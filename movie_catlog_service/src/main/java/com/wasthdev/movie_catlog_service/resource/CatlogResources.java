fba01981aaaac0f85142daa5231487612c5a174b
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2021-11-12 13:49:34 +0530

    add webclient Builder

--- movie_catlog_service/src/main/java/com/wasthdev/movie_catlog_service/resource/CatlogResources.java
+++ movie_catlog_service/src/main/java/com/wasthdev/movie_catlog_service/resource/CatlogResources.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -23,15 +24,29 @@ public class CatlogResources {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatlogItem> getCatalog(@PathVariable("userId") String userId){
+
+
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234",5),
                 new Rating("124",5)
         );
        return ratings.stream().map(rating -> {
                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieID(), Movie.class);
-                  return new CatlogItem(movie.getName(),"desc", rating.getRating());
+
+                 /*
+                   Movie movie = webClientBuilder.build()
+                           .get()
+                           .uri("http://localhost:8082/movies/" + rating.getMovieID())
+                           .retrieve()
+                           .bodyToMono(Movie.class)
+                           .block();
+                   */
+                   return new CatlogItem(movie.getName(),"desc", rating.getRating());
        })
                 .collect(Collectors.toList());
 
